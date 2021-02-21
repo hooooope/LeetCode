af1e5fd1f2abcc7d1967c02782ca209433fe768f
hope <1962926166@qq.com>
2021-02-21 21:28:13 +0800

    largeGroupPositions

---
+++ Easy/830. 较大分组的位置.js
@@ -0,0 +1,46 @@
+/**
+ * 在一个由小写字母构成的字符串 s 中，包含由一些连续的相同字符所构成的分组。
+ * 例如，在字符串 s = "abbxxxxzyy" 中，就含有 "a", "bb", "xxxx", "z" 和 "yy" 这样的一些分组。
+ * 分组可以用区间 [start, end] 表示，其中 start 和 end 分别表示该分组的起始和终止位置的下标。
+ * 上例中的 "xxxx" 分组用区间表示为 [3,6] 。
+ * 我们称所有包含大于或等于三个连续字符的分组为 较大分组 。
+ * 找到每一个 较大分组 的区间，按起始位置下标递增顺序排序后，返回结果。
+ *
+ * 示例 1：
+ * 输入：s = "abbxxxxzzy"
+ * 输出：[[3,6]]
+ * 解释："xxxx" 是一个起始于 3 且终止于 6 的较大分组。
+ *
+ * 示例 2：
+ * 输入：s = "abc"
+ * 输出：[]
+ * 解释："a","b" 和 "c" 均不是符合要求的较大分组。
+ *
+ * 示例 3：
+ * 输入：s = "abcdddeeeeaabbbcd"
+ * 输出：[[3,5],[6,9],[12,14]]
+ * 解释：较大分组为 "ddd", "eeee" 和 "bbb"
+ */
+
+/**
+* @param {string} s
+* @return {number[][]}
+*/
+var largeGroupPositions = function (s) {
+    const result = []
+    let count = 1
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {// 发现重复项则 count + 1
+            count++
+        } else {
+            if (count >= 3) {// 重复项有 3 个及以上
+                result.push([i - count, i - 1])
+            }
+            count = 1// 重置 count
+        }
+    }
+    if (count >= 3) {// 处理最后有可能存在的较大分组。
+        result.push([s.length - count, s.length - 1])
+    }
+    return result
+};
